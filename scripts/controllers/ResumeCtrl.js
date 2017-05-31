efae5446961f854e69698d05db4158e2ff192315
ZSTRAD44 <zachstradling@gmail.com>
2017-05-31 16:57:35 -0700

    updates

--- scripts/controllers/ResumeCtrl.js
+++ scripts/controllers/ResumeCtrl.js
@@ -1,7 +1,7 @@
 angular
   .module('portApp')
   .controller('ResumeCtrl', ['$scope', function($scope) {
-    $scope.pdfUrl = '/assets/docs/zchary_stradling_resume_2017_color.pdf';
-    $scope.colorResume = '/assets/docs/zchary_stradling_resume_2017_color.pdf';
-    $scope.bwResume = '/assets/docs/zchary_stradling_resume_2017_bw.pdf';
+    $scope.pdfUrl = '/assets/docs/zachary_stradling_resume_2017_color.pdf';
+    $scope.colorResume = '/assets/docs/zachary_stradling_resume_2017_color.pdf';
+    $scope.bwResume = '/assets/docs/zachary_stradling_resume_2017_bw.pdf';
   }]);
